528a88510b64ff26daa3bf27dd414efc920ffb6a
Saurabh <saurabhkawatra2001@gmail.com>
2025-08-18 20:13:25 +0530

    updation of docker and some minot changes

--- EMS/tests/test_api_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-from django.shortcuts import render
-url = "http://127.0.0.1:8000/api/dashboard/"
-headers = {
-    "Authorization": "Token f1633fd99190402551fd2b9bd750020908a188bb"
-}
-
-response = requests.get(url, headers=headers)
-print(response.request.headers)
